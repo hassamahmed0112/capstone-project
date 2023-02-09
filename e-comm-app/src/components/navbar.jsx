24b9e472366a5fe39b693814f1d1e7211eb8270e
hassamahmed0112 <hassam.ahmed0112@live.com>
2023-02-09 22:29:15 +0500

    Initial commit

---
+++ e-comm-app/src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import { Cat } from "phosphor-react";
+import "./navbar.css"
+
+export const Navbar = () => {
+  return ( 
+   <div className="navbar">
+    <div className="links">
+      
+        <Link to="/"> Shop </Link>
+
+        <Link to="./cart"> 
+
+        <Cat size={32} /> 
+
+        </Link>
+        
+        
+
+    </div>
+  </div> 
+  );
+};
---
+++ e-comm-app/src/products.js
@@ -0,0 +1,46 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.jpeg";
+import product3 from "./assets/3.jpeg";
+import product4 from "./assets/4.jpeg";
+import product5 from "./assets/5.jpeg";
+import product6 from "./assets/6.jpeg"
+
+export const PRODUCTS = [
+    {
+        id : 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id : 2,
+        productName: "Iwatch",
+        price: 489.0,
+        productImage: product2,
+    },
+    {
+        id : 3,
+        productName: "Airpods",
+        price: 329.0,
+        productImage: product3,
+    },
+    {
+        id : 4,
+        productName: "DSLR",
+        price: 899.0,
+        productImage: product4,
+    },
+    {
+        id : 5,
+        productName: "Imac",
+        price: 2999.0,
+        productImage: product5,
+    },
+    {
+        id : 6,
+        productName: "car",
+        price: 99999.0,
+        productImage: product6,
+    },
+
+];
